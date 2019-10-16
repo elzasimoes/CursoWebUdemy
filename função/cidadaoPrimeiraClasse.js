3b8027d974121266de38586bdc5419c5ed6ea2ef
Elza Simões <elzaesimoes@gmail.com>
2019-10-16 09:47:33 -0400

    16/10

---
+++ função/cidadaoPrimeiraClasse.js
@@ -0,0 +1,10 @@
+//Função em Js é First-Class Object (Citizens)
+//Higher-order function
+//Criar de forma literal
+
+function fun1() {}
+
+
+// Armazenar em uma variável
+
+const fun2 = function() {}
