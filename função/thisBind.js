75841f190d1f2a534d5e5087341a4a7665585567
Elza Simões <elzaesimoes@gmail.com>
2019-10-17 10:45:19 -0400

    17/10

---
+++ função/thisBind.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    saudacao: 'Bom dia!!!!!!!!!!!!!!!!!!!!!!!!!!',
+    falar(){
+        console.log(this.saudacao)
+    }
+}
+
+pessoa.falar()
+const falar = pessoa.falar
+falar() //Conflito entre paradigmas: funcional e oo
+
+const falardePessoa = pessoa.falar.bind(pessoa)
+falardePessoa()
+
