4cb64182fd56183abac86ad01cefb150f63292ff
Elza Simões <elzaesimoes@gmail.com>
2019-10-18 11:18:51 -0400

    18/10

--- função/arrowFunction.js
+++ função/arrowFunction.js
@@ -5,3 +5,14 @@ let dobro = function (a) {
 dobro = (a) => {
     return 2 * a
 }
+
+dobro = a => 2 * a //return implicito
+console.log(dobro(Math.PI))
+
+let ola = function () {
+    return 'Olá'
+}
+
+ola = () => 'Olá'
+ola = _ => 'Olá' //possui um param
+console.log(ola())
