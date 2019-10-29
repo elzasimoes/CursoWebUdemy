6340f2e60502adf4e7c199e27eca104f782746da
Elza Simões <elzaesimoes@gmail.com>
2019-10-29 11:23:34 -0400

    conceitos oo

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+//Não é um condigo executável
+
+// Procedural:Procedimento que reutiliza em vários trechos
+processamento(valor1, valor2, valor3)
+
+// 00
+
+objeto = {
+    valor1, 
+    valor2, 
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento()
+
+
+// Principíos importantes
+// Abstração
+// Encapsulamento
