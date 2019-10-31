f13d9b4fcbd0ddfc1732c963b732f60478938573
Elza Simões <elzaesimoes@gmail.com>
2019-10-31 12:05:47 -0400

    módulos node

---
+++ node/moduloA.js
@@ -0,0 +1,3 @@
+this.ola = 'Fala pessoa'
+exports.Bemvindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até o próximo exemplo'
---
+++ objeto/criandoObjetos.js
@@ -0,0 +1,14 @@
+//usando notação literal
+const obj1 = {}
+console.log(obj1)
+
+//Object em JS
+console.log(typeof Object, typeof New Object)
+const obj2 = new Object
+console.log(obj2)
+
+//Função construtura
+
+function (property) Produto.nome: any
+    this.nome = nome
+}
