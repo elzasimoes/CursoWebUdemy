3fcb6ab6149a862db661f49eac6735f2dbeeadcc
Elza Simões <elzaesimoes@gmail.com>
2019-11-01 13:21:13 -0400

    objetos

--- objeto/criandoObjetos.js
+++ objeto/criandoObjetos.js
@@ -3,12 +3,45 @@ const obj1 = {}
 console.log(obj1)
 
 //Object em JS
-console.log(typeof Object, typeof New Object)
+console.log(typeof Object, typeof new Object)
 const obj2 = new Object
 console.log(obj2)
 
 //Função construtura
 
-function (property) Produto.nome: any
-    this.nome = nome
+function Produto(nome, preco, desc) {
+    this.nome
+    this.getPrecoComDesconto = () => {
+        return preco * (1 - desc)
+    }
 }
+
+const p1 = new Produto('Caneta', 7.99, 0.15)
+const p2 = new Produto('Notebook', 2998.99, 0.25)
+console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+// Função Factory
+function criarFuncionario(nome, salarioBase, faltas){
+    return {
+        nome,
+        salarioBase,
+        faltas,
+        getSalario(){
+            return (salarioBase / 30) * (30 - faltas)
+        }
+    }
+}
+const f1 = criarFuncionario('Elza', 600, -5)
+const f2 = criarFuncionario('Maria', 11400, 1)
+console.log(f1.getSalario(), f2.getSalario())
+
+// Object.create
+
+const filha = Object.create(null)
+filha.nome = 'Ana'
+console.log(filha)
+
+// função famosa que retorna objeto...
+
+const fromJSON = JSON.parse('{"info": "Sou um JSON"}')
+console.log(fromJSON.info)
