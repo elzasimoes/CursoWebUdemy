6af40aa86da2b4b266c100d484aa1da3e6e7c7f5
Elza Simões <elzaesimoes@gmail.com>
2019-11-06 16:23:04 -0400

    módulo node

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,9 +2,25 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
 
-app.get('/produtos', (req, res, next) =>{
-    res.send({ nome: 'Notebook', preco: 123.45 }) //Converter para JSON
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos()) //Converter para JSON
+})
+
+app.get('/produtos/:id', (req, res, next) =>{
+    res.send(bancoDeDados.getProdutos(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) =>{
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
