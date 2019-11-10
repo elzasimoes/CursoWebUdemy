939d2157ec5a7e91243dc23fcf0790d2f078314d
Elza Simões <elzaesimoes@gmail.com>
2019-11-10 16:40:20 -0400

    html

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -4,7 +4,14 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: true}))
 
-app.post('usuarios', (req, resp) =>{
+
+
+app.post('/usuarios', (req, resp) =>{
+    console.log(req.body)
+    resp.send('<h1>Parabéns!!!!</h1>')
+})
+
+app.post('/usuarios/3', (req, resp) => {
     console.log(req.body)
     resp.send('<h1>Parabéns!!!!</h1>')
 })
