802c8769082314a717cc8153345d1a095cda9fef
Elza Simões <elzaesimoes@gmail.com>
2019-11-24 15:47:22 -0200

    módulo javascript completo

---
+++ javascript/fundamentos/variaveisEconstantes.js
@@ -0,0 +1,19 @@
+var a = 3 //atruição da variável a = identificação 3 = valor
+let b = 4 //criar variáveis let, forma mais moderna de trabalhar c variáveis
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+//c = 50
+
+console.log(c)
+
+//módulo fundamentos completo
