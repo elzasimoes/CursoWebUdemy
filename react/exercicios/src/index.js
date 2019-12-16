ecfb0f9c4d957ba534096098cf28dfa2561fbf3b
Elza Simões <elzaesimoes@gmail.com>
2019-12-16 13:23:44 -0500

    react

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Extensão JSX
+// React Dom, conversa com a página
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDom.render(<h1>Primeira App em React</h1>, document.getElementById('root'))
