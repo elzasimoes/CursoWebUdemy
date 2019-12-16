5bad4a7e955a6773b31212f253378bdfe7cdc55f
Elza Simões <elzaesimoes@gmail.com>
2019-12-16 16:58:38 -0500

    Projeto Calculadora Finalizado

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+import BomDia from './componentes/BomDia'
 //Extensão JSX
 // React Dom, conversa com a página
 
-ReactDom.render(<h1>Primeira App em React</h1>, document.getElementById('root'))
+ReactDom.render(<BomDia/>, document.getElementById('root'))
