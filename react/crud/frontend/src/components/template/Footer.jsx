a20d3e53994e471061bd874ed9261d9c883ab7a9
Elza Simões <elzaesimoes@gmail.com>
2019-12-16 20:14:17 -0500

    projeto crud

---
+++ react/crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+    <footer className="footer">
+        <span>
+            Desenvolvido <i className="fa fa-heart text-danger"></i> por
+            <strong> Elza</strong> <span className="text-danger">Simões.</span>
+        </span>
+    </footer>
